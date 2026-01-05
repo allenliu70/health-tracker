c97e1a1602a2b1087001744777312b799c53d75b
Allen Liu <allenliu70@gmail.com>
2026-01-05 15:32:37 -0800

    updated packages.json and simplified Dockerfile.dev.

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
         <h2>Vite + React!</h2>
       </div>
-      <h1>Hello, Tim!</h1>
+      <h1>Hello, Tian-tzu Liu! Happy 2026!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
